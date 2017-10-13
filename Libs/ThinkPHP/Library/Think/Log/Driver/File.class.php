1af56e5e89dc296b909f4c7d0c5f4c6055e057bb
zhqagp <zhqagp@aliyun.com>
2017-10-13 18:11:02 +0800

    fix

--- Libs/ThinkPHP/Library/Think/Log/Driver/File.class.php
+++ Libs/ThinkPHP/Library/Think/Log/Driver/File.class.php
@@ -40,7 +40,7 @@ class File {
         $log_dir = dirname($destination);
         if (!is_dir($log_dir)) {
             mkdir($log_dir, 0755, true);
-        }
+        }        
         //检测日志文件大小，超过配置大小则备份日志文件重新生成
         if(is_file($destination) && floor($this->config['log_file_size']) <= filesize($destination) ){
             rename($destination,dirname($destination).'/'.time().'-'.basename($destination));
